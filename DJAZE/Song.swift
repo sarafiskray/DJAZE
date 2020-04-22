c1143cc0a2c355d6cb56f29a7c71235cc53edd84
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-04-22 13:11:29 -0400

    sorted song algorithm

---
+++ DJAZE/Song.swift
@@ -0,0 +1,27 @@
+//
+//  Song.swift
+//  DJAZE
+//
+//  Created by Bo Warren on 4/22/20.
+//  Copyright © 2020 DJAZE. All rights reserved.
+//
+
+import UIKit
+
+class Song: Codable {
+    
+    var title: String
+    var artist: String
+    var upVoteCount: Int
+    var downVoteCount: Int
+    var aggVote: Int
+    
+    init(title: String, artist: String, upVoteCount: Int, downVoteCount: Int) {
+        self.title = title
+        self.artist = artist
+        self.upVoteCount = upVoteCount
+        self.downVoteCount = downVoteCount
+        self.aggVote = upVoteCount - downVoteCount
+    }
+
+}
--- DJAZE/UserViewController.swift
+++ DJAZE/UserViewController.swift
@@ -11,15 +11,18 @@ import SpotifyKit
 
 class UserViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var songs = ["Peta", "Gorgeous", "Many Men"]
-    var artists = ["Roddy Ricch", "Kanye West", "50Cent"]
+    var songs = [Song(title: "Peta", artist: "Roddy Ricch", upVoteCount: 6, downVoteCount: 15), Song(title: "Gorgeous", artist: "Kanye West", upVoteCount: 20, downVoteCount: 10), Song(title: "Many Men", artist: "50Cent", upVoteCount: 5, downVoteCount: 2)]
+    
+    lazy var sortedSongs = songs.sorted(by: {$0.aggVote > $1.aggVote})
     
     @IBOutlet weak var nowPlayingSongLabel: UILabel!
     @IBOutlet weak var nowPlayingArtistLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return songs.count
@@ -27,8 +30,8 @@ class UserViewController : UIViewController, UITableViewDelegate, UITableViewDat
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath) as! SongTableViewCell
-        cell.songLabel.text = songs[indexPath.row]
-        cell.artistLabel.text = artists[indexPath.row]
+        cell.songLabel.text = sortedSongs[indexPath.row].title
+        cell.artistLabel.text = sortedSongs[indexPath.row].artist
         return cell
     }
     
