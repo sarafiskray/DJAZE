395d8b65285108a30f9d4a7ea79ef5dc4f3bcb4e
Saraf Ray <sarafiskray@gmail.com>
2020-04-29 22:17:41 -0400

    aesthetic changes - dark mode

--- DJAZE/ViewController.swift
+++ DJAZE/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         // Authorize our app for the Spotify account if there is no token
         // This opens a browser window from which the user can authenticate into his account
         spotifyManager.authorize()
-        //loadUser()
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     @IBAction func DJSegueButton(_ sender: UIButton) {
